8a7733e7bf85072df5d60df77407f9b170002e9f
Hamiric <do9870@naver.com>
2024-11-29 19:57:29 +0900

    Fix : "홈화면 컨트롤러 연결"
    reslove: #1

--- lib/ui/pages/homepage/homepage.dart
+++ lib/ui/pages/homepage/homepage.dart
@@ -9,26 +9,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    _searchController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: '지역이름검색'
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: '지역이름검색'
+            ),
+            onChanged: (text) {
+              print(text);
+            },
           ),
         ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: ListView.separated(
-            itemCount: 4,
-            itemBuilder:(context, index) {
-              return LocalSearchBox();
-            },
-            separatorBuilder: (context, index) => SizedBox(height: 20,),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: ListView.separated(
+              itemCount: 4,
+              itemBuilder:(context, index) {
+                return LocalSearchBox();
+              },
+              separatorBuilder: (context, index) => SizedBox(height: 20,),
+            ),
           ),
         ),
       ),
