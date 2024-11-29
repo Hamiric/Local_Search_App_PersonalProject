9d754855725040ac1e244d15d1a3205ae1e85d56
Hamiric <do9870@naver.com>
2024-11-29 19:50:55 +0900

    Feat : "홈페이지, 세부페이지 UI 완성"

--- lib/ui/pages/homepage/homepage.dart
+++ lib/ui/pages/homepage/homepage.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:local_search_app_personalproject/ui/pages/homepage/widgets/local_search_box.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: '지역이름검색'
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: ListView.separated(
+            itemCount: 4,
+            itemBuilder:(context, index) {
+              return LocalSearchBox();
+            },
+            separatorBuilder: (context, index) => SizedBox(height: 20,),
+          ),
+        ),
+      ),
     );
   }
 }
